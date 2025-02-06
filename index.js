804d835a831c11b19d5b4a32ec9ebbbd30546623
shrinidhialur21 <shrinidhialur21@example.com>
2025-02-06 09:16:33 +0000

    commented-headers

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ const run = async () => {
       try {
         // Make the HTTP request to ksqlDB
         const response = await axios.post('http://4.186.36.38:8088/info',  {
-          headers: {
-            'Content-Type': 'application/vnd.ksql.v1+json',
-          },
+          // headers: {
+          //   'Content-Type': 'application/vnd.ksql.v1+json',
+          // },
         });
 
         // Log the response from ksqlDB
